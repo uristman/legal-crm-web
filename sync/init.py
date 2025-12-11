0ea62d0a67719d4b01399849347d8e26891e13ea
uristman <64069411+uristman@users.noreply.github.com>
2025-12-11 10:31:20 +0400

    Update init.py

--- sync/init.py
+++ sync/init.py
@@ -1,14 +1,1 @@
-"""
-Пакет синхронизации Legal CRM с Яндекс.Диском
-Обеспечивает автоматическое резервное копирование и синхронизацию данных
-"""
-
-from .yandex_webdav import YandexDiskWebDAV, DatabaseSyncManager
-from .sync_manager import SyncManager, SyncConfiguration
-
-__all__ = [
-    'YandexDiskWebDAV',
-    'DatabaseSyncManager', 
-    'SyncManager',
-    'SyncConfiguration'
-]
+# Sync package for Yandex Disk integration
